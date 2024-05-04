d19d895550e0e87c415934231fce1c55e03793c3
Aaron <aaronli5260@gmail.com>
2024-05-03 21:19:57 -0400

    banner

--- src/components/home/HeaderSection/HeaderSection.tsx
+++ src/components/home/HeaderSection/HeaderSection.tsx
@@ -2,9 +2,14 @@ import Banner from '@/components/shared/Banner/Banner'
 import styles from './HeaderSection.module.css'
 
 const HeaderSection = () => {
+    const currentBanner: Banner = {
+        message: 'New Course: Metro Boomin',
+        link: 'https://skool.com/buildingbeats/metro'
+    }
+
     return (
         <div className={styles.liner}>
-            <Banner message={"New Course: Metro Boomin"}/>
+            <Banner {...currentBanner}/>
         </div>
     )
 }
--- src/components/shared/Banner/Banner.tsx
+++ src/components/shared/Banner/Banner.tsx
@@ -1,14 +1,27 @@
 import styles from './Banner.module.css'
+import { Banner } from '@/utils/types/Banner'
+import Link from 'next/link'
 
-interface Props {
-    message: string,
-}
 
-const Banner: React.FC<Props> = ({ message }) => {
+const Banner: React.FC<Banner> = ({ message, link }) => {
+    const fullMessage = message.toUpperCase()
+    const messages = Array.from({ length: 10 }, () => fullMessage)
+
     return (
         <div className={styles.bannerContainer}>
-            <div className={styles.rotatingText}>
-                {message}
+            <div className={styles.slidingText}>
+                {messages.map((msg, index) => (
+                    <Link className={styles.bannerMessage} key={index} href={link}>
+                        {msg}
+                    </Link>
+                ))}
+            </div>
+            <div className={styles.slidingText}>
+                {messages.map((msg, index) => (
+                    <Link className={styles.bannerMessage} key={index} href={link}>
+                        {msg}
+                    </Link>
+                ))}
             </div>
         </div>
     )
