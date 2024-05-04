5ec859a2b45887612e1c2ed2fd623064deaea4d3
Aaron <aaronli5260@gmail.com>
2024-05-04 10:36:39 -0400

    searchbar

---
+++ src/components/home/SearchSection/SearchSection.tsx
@@ -0,0 +1,21 @@
+import styles from './SearchSection.module.css'
+import logo from '../../../../public/logolong.png'
+import Image from 'next/image'
+import { IoSearch } from "react-icons/io5"
+
+const SearchSection = () => {
+
+    return (
+        <div className={styles.liner}>
+            <div className={styles.logoSection}>
+                <Image src={logo} alt='Building Beats logo' loading="eager" width={452} />
+            </div>
+            <div className={styles.searchbarSection}>
+                <input className={styles.searchbar} />
+                <IoSearch size={34} />
+            </div>
+        </div>
+    )
+}
+
+export default SearchSection
